8b831e2383e3db980f5f174386db15e269cf21bb
n14s <n14x@posteo.de>
2022-03-03 14:15:40 +0100

    add hot changing configs

--- src/main/java/javadummy/JavaDummy.java
+++ src/main/java/javadummy/JavaDummy.java
@@ -7,13 +7,20 @@ import java.io.FileReader;
 
 public class JavaDummy {
     public static void main(String[] args) {
-        try {
-            Object obj = new JSONParser().parse(new FileReader("config.json"));
-            JSONObject jo = (JSONObject) obj;
-            String dings = (String) jo.get("foo");
-            System.out.println(dings);
-        } catch (Exception e) {
-            System.out.println(e);
+        while (true) {
+            try {
+                Object obj = new JSONParser().parse(new FileReader("config.json"));
+                JSONObject jo = (JSONObject) obj;
+                String dings = (String) jo.get("foo");
+                System.out.println(dings);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            try {
+                Thread.sleep(5000);
+            } catch(Exception e) {
+                System.out.println(e);
+            }
         }
     }
 }
